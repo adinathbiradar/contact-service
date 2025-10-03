d3febcfbcfd85fc73a1173fb144e9011e53e58cd
adinathbiradar <adinathbiradar77@gmail.com>
2025-10-03 16:42:26 +0530

    updated

--- src/main/java/in/main/controller/ContactController.java
+++ src/main/java/in/main/controller/ContactController.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/contacts")
+@RequestMapping("/api/contacts")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ContactController {
 
